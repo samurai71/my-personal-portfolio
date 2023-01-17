9b22a1945b27b88ec8fcaf1a40c1c2a8ab68652e
Mark Landeryou <samurai96@hotmail.com>
2023-01-16 20:02:00 -0500

    style:made some changes with style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 //import Image from 'next/image'
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-800 text-center py-2">
+      Hello world!!!
+    </h1>
+  );
 }
