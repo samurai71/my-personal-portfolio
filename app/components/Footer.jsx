2620504a78d5e65e5be40f0565a1d4e2184d2ac1
Mark Landeryou <samurai96@hotmail.com>
2023-02-28 08:41:44 -0500

    refactor: edited the footer component added the footer tag

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex justify-center w-full">
-      <p className="font-mono">
-        &copy; {new Date().getFullYear()} Mark Landeryou All rights reserved.
-      </p>
-    </div>
+    <footer>
+      <div className="flex justify-center w-full">
+        <p className="font-mono">
+          &copy; {new Date().getFullYear()} Mark Landeryou All rights reserved.
+        </p>
+      </div>
+    </footer>
   );
 }
 
