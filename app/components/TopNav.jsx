d047bce9b2d0c4b2d3b82a35ee5a60cef996bbb7
Mark Landeryou <samurai96@hotmail.com>
2023-03-20 02:19:54 -0400

    style: added padding bottom Mark Landeryou <samurai96@hotmail.com>

--- app/components/TopNav.jsx
+++ app/components/TopNav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function TopNav() {
-  return <div className="text-white pt-2 pt-2">TopNav</div>;
+  return <div className="text-white pt-2 pb-2">TopNav</div>;
 }
 
 export default TopNav;
