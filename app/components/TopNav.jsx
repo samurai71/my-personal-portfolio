9bf56450a36c6d436de2d4497c4900819ae24783
Mark Landeryou <samurai96@hotmail.com>
2023-05-06 08:47:44 -0400

    refactor: symantically fixed topnav component

--- app/components/TopNav.jsx
+++ app/components/TopNav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function TopNav() {
-  return <div className="text-white pt-2 pb-2">TopNav</div>;
+  return <nav className="text-white pt-2 pb-2">TopNav</nav>;
 }
 
 export default TopNav;
